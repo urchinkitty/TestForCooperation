9b89b02ccd91e5a23d9c15b3b27d85985d7cc634
Jude <urchinkitty@naver.com>
2022-08-19 12:36:25 +0900

    First Man modified

--- CompanyProject.c
+++ CompanyProject.c
@@ -6,9 +6,10 @@ int main(void)
     printf("Lest's do our job individually.");
 
     // Major Member
-
+    printf("I'm M. I did my job as well when A is working.");
+    
     // Minor Member
 
     // Result
-
+    
 }
