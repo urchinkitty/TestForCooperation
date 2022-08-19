fbb8f45ac972de7b4da1272bd0c51ceaf8cca019
Jude <urchinkitty@naver.com>
2022-08-19 11:07:42 +0900

    First Man Commit

---
+++ CompanyProject.c
@@ -0,0 +1,14 @@
+#include"CompanyMembers"
+
+int main(void)
+{
+    printf("Hello, This is our company project!");
+    printf("Lest's do our job individually.");
+
+    // Major Member
+
+    // Minor Member
+
+    // Result
+
+}
